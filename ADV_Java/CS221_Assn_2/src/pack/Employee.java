f10181b2fe30f7160bd8cae17aef8e481b5d6ebb
Frostmaine <myackiel97@gmail.com>
2021-10-03 21:58:22 -0400

    Changes to be committed: 	modified:   ADV_Java/CS221_Assn_2/src/pack/Customer.java 	modified:   ADV_Java/CS221_Assn_2/src/pack/Employee.java 	deleted:    C_Assembly/hello 	deleted:    C_Assembly/hello.c 	deleted:    C_Assembly/hellothreads 	deleted:    C_Assembly/hellothreads.c 	ADV_Java/CS221_Assn_2/src/pack/Person.java 	ADV_Java/CS221_Assn_3/ 	ADV_Java/CS221_Classes/ 	C_Assembly/ 	Polisci/

--- ADV_Java/CS221_Assn_2/src/pack/Employee.java
+++ ADV_Java/CS221_Assn_2/src/pack/Employee.java
@@ -13,13 +13,10 @@ import java.util.Objects;
  *      
  * @author Matthew Yackiel
  */
-public class Employee
+public class Employee extends Person
 {
     private final int id;
-    private final Name name;
-    private final Address address;
-    private final Compensation compensation;
-    private final LocalDate birthday;
+    private Compensation compensation;
     private final LocalDate employee_since;
     
     /**
@@ -32,9 +29,7 @@ public class Employee
     public Employee(Name name, Address address, Compensation compensation,
             LocalDate birthday)
     {
-        this.name = name;
-        this.address = address;
-        this.birthday = birthday;
+        super(name, address, birthday);
         this.compensation = compensation;
         this.employee_since = LocalDate.now();
         this.id = Math.abs(this.hashCode()); // a extremely likely unique value
@@ -53,10 +48,8 @@ public class Employee
     private Employee(Name name, Address address, Compensation compensation, 
             LocalDate birthday, LocalDate employee_since, int id)
     {
-            this.name = name;
-            this.address = address;
+            super(name, address, birthday);
             this.compensation = compensation;
-            this.birthday = birthday;
             this.employee_since = employee_since;
             this.id = id;
     }
@@ -65,71 +58,23 @@ public class Employee
      * Give an employee a pay raise
      * @param compensation the new compensation which must have a higher value 
      *                     than this.compensation
-     * @return returns the calling object if it is not greater than the current
-     *          compensation.  Otherwise returns a new Employee object reflecting
-     *          reflecting the changed compensation
      */
-    public Employee payRaise(Compensation compensation)
+    public void payRaise(Compensation compensation)
     {
         // null check
         if (this == null || compensation == null)
-            return this;
-        // Check if the change is valid
-        if(compensation.compareTo(this.compensation) <= 0)
-            return this;
-        // return a new Employee object reflecting the change.
-        return new Employee(name, address, compensation, birthday, 
-                            employee_since, id);
+            return;
+        if (this.compensation.compareTo(compensation) >= 0)
+            return;
+        this.compensation = compensation;
     }
     
-    /**
-     * Change the address of an employee
-     * @param address the new address of the employee
-     */
-    public Employee changeAddress(Address address)
-    {
-        // Null check
-        if (this == null || address == null)
-            return this;
-        return new Employee(name, address, compensation, birthday, 
-                            employee_since, id);
-    }
-    
-    /**
-     * Change the name of an employee
-     * @param name the new name the employee will have
-     */
-    public Employee changeName(Name name)
-    {
-        // null and equals check
-        if (this == null || name == null || this.name.equals(name))
-            return this;
-        return new Employee(name, address, compensation, birthday, 
-                            employee_since, id);
-    }
-
     /**
      * @return the employee's unique id
      */
     public int getId() 
     {
         return id;
-    }
-
-    /**
-     * @return the employee's name
-     */
-    public Name getName() 
-    {
-        return name;
-    }
-
-    /**
-     * @return the employee's address
-     */
-    public Address getAddress() 
-    {
-        return address;
     }
     
     /**
@@ -138,14 +83,6 @@ public class Employee
     public Compensation getCompensation() 
     {
         return compensation;
-    }
-
-    /**
-     * @return the employee's birthday
-     */
-    public LocalDate getBirthday() 
-    {
-        return birthday;
     }
 
     /**
@@ -164,9 +101,7 @@ public class Employee
     public int hashCode() 
     {
         int hash = 5;
-        hash = 61 * hash + Objects.hashCode(this.name);
-        hash = 61 * hash + Objects.hashCode(this.address);
-        hash = 61 * hash + Objects.hashCode(this.birthday);
+        hash = 61 * hash + super.hashCode();
         return hash;
     }
 
@@ -185,12 +120,6 @@ public class Employee
         if (getClass() != obj.getClass()) 
             return false;
         final Employee other = (Employee) obj;
-        if (!Objects.equals(this.name, other.name)) 
-            return false;
-        if (!Objects.equals(this.address, other.address)) 
-            return false;
-        if (!Objects.equals(this.birthday, other.birthday)) 
-            return false;
         return Objects.equals(this.employee_since, other.employee_since);
     }
 
@@ -200,37 +129,7 @@ public class Employee
     @Override
     public String toString() 
     {
-        return "Employee{" + "id=" + id + ", name=" + name + ", address=" + 
-                address + ", compensation=" + compensation + ", birthday=" + 
-                birthday + ", employee_since=" + employee_since + '}';
-    }
-    
-    public static void main(String[] args) 
-    {
-        Employee emp = new Employee(new Name("Matthew", "Yackiel", "Thomas"), 
-                new Address("557 Clints Lane", "Bloomsburg", "PA", "17815"),
-                new Compensation(30_000, true), LocalDate.of(1997, 3, 31));
-        
-        // The initial object
-        System.out.println(emp);
-        
-        
-        // Check pay raise method
-        Compensation pay_raise = new Compensation(31000, true);
-        Compensation pay_drop = new Compensation(29000, true);
-        emp = emp.payRaise(pay_raise);
-        System.out.println(emp);
-        emp = emp.payRaise(pay_drop);
-        System.out.println(emp);
-        
-        // Getters
-        System.out.println("Name: " + emp.getName());
-        System.out.println("Address: " + emp.getAddress());
-        System.out.println("Birthday: " + emp.getBirthday());
-        System.out.println("Compensation: " + emp.getCompensation());
-        System.out.println("Employee_Since: " + emp.getEmployee_since());
-        System.out.println("Employee Id: " + emp.getId());
-        
-        
+        return "Employee{" + "id=" + id + "compensation=" + compensation 
+                + ", employee_since=" + employee_since + '}';
     }
 }
