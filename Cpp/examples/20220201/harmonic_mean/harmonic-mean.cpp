a9e6f9e8761ef34119b37a9a6953f0d36c50e31a
Frostmaine <myackiel97@gmail.com>
2022-02-01 11:32:36 -0500

    Added code changes for end of class on 20220201

--- Cpp/examples/20220201/harmonic_mean/harmonic-mean.cpp
+++ Cpp/examples/20220201/harmonic_mean/harmonic-mean.cpp
@@ -3,21 +3,24 @@
 //     their harmonic mean
 
 #include <iostream>
+#include <vector>
 #include "harmean.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-  int size = atoi(argv[1]);
-  double data[size];
+  vector<double> values;
   do {
-    cin >> data[i]; // need a vector class
+    double tmp;
+    cin >> tmp; // need a vector class, basically a variable length array
     if (cin.eof()) // scan to end of file
       break;
+    values.push_back( tmp );
   } while(true);
 
-  double hm = harmonic_mean(data, size);
+  cout << values.size() << endl;
+  double hm = harmonic_mean(values.data(), values.size());
   cout << hm << endl;
   return EXIT_SUCCESS;
 }
