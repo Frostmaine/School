c0f5277b72eb07fb52caa057d99985e3e38a4eb7
Frostmaine <myackiel97@gmail.com>
2022-02-01 11:45:41 -0500

    Added robomaze header file

---
+++ Cpp/examples/20220201/maze/robomaze.h
@@ -0,0 +1,23 @@
+// robomaze.h
+//    declaration for a robot class that can
+//    be steered through a maze
+// pre-condition:
+//    a valid maze has been created, and the robot
+//    can use the maze to "understand" where it is
+// post-condition:
+//    root will have found its way to the lower right.
+// value semantics:
+//    robot can be copied or assigned
+#ifndef _ROBOMAZE_ // pre processor to ensure class is not defined twice
+#define _ROBOMAZE_
+
+class robomaze {
+  
+public:
+  
+private:
+  
+};
+
+
+#endif
